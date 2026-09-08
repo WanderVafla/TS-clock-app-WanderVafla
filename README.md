1628d93c0453d43f3dfef60d52b7d39b41b01eb4
WanderVafla <procenkovladik1@gmail.com>
2026-09-08 11:34:18 +0200

    chore: initial Elysia backend skeleton

--- README.md
+++
@@ -1,1 +0,0 @@
-# TS-clock-app-WanderVafla
---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen({
+  port: 3000,
+  hostname: "0.0.0.0"
+});
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
