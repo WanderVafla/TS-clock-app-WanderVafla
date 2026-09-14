e1da8b23914d4e5c4744c46fc63e4a84717f0793
WanderVafla <procenkovladik1@gmail.com>
2026-09-14 09:15:19 +0200

    fix(schemaSQL): project_id in time_entries is obligatory

--- init.sql
+++ init.sql
@@ -9,7 +9,7 @@ create table if not exists time_entries (
     start_time timestamptz not null,
     end_time timestamptz check (end_time >= start_time),
     description varchar(255),
-    project_id int,
+    project_id int not null,
     
     foreign key (project_id) references projects(id) on delete cascade
 );
